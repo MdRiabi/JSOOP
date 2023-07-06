56723b4ea86c98cbadbcef477d1d40ed93af2995
MdRiabi <mhdriabi2013@gmail.com>
2023-07-06 14:47:50 +0200

    adding new class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,19 +1,41 @@
-const productList = {
-    products: [
-        {title:'A Pillow',
-        imageUrl:'https://th.bing.com/th/id/R.551b3f502f6c089c36c19e843c188dc9?rik=7%2brlsXe3dl0Bqw&riu=http%3a%2f%2f1.bp.blogspot.com%2f-QvL28cHZ-jw%2fU6xv6wD48KI%2fAAAAAAAAiy0%2fvTqlIJXTwp8%2fs1600%2fpillows%2b5.jpg&ehk=ZGjGWU1qByooSu%2fNM%2b0iWWs%2bub3FgJk0%2bb6BexNaSOk%3d&risl=&pid=ImgRaw&r=0',
-        price:19.99,
-        description:'a product description'
-    },
-        {title:'A fidora cap',
-        imageUrl:'https://th.bing.com/th/id/R.95dc5e66f6bed801dd0f00749244ae5b?rik=jPRBIdbTC1p0nQ&riu=http%3a%2f%2fwww.vineos.fr%2fvins%2fofficiel%2fvetement-sport-nike-femme-pas-cher-yxtonu.jpg&ehk=6aVUywyu%2fQ2o8ZaBCDg%2b%2fNmDJZHMbLVhpDaDnQm1Yvo%3d&risl=&pid=ImgRaw&r=0',
-        price:29.99,
-        description:'a product description'},
-        {title:'A short ',
-        imageUrl:'https://th.bing.com/th/id/R.308485418adf4d8b3d57e247a81e7ddf?rik=v%2fO6ENV01xmf9g&riu=http%3a%2f%2fwww.college-malraux-pontarlier.fr%2fnouvelle%2fnike-jordan-pour-fille---3.jpg&ehk=%2bnpkxwNx8W8r5ZQOtZ6A63YpqqrOpicVgxv%2bRUROh24%3d&risl=&pid=ImgRaw&r=0',
-        price:49.99,
-        description:'a product description'}
-],
+class Product {
+//title = 'DEFAULT';
+//imageUrl;
+//price;
+
+//description;
+
+constructor(title, imageUrl, price, description)
+ {
+
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+
+}
+}
+
+class ProductList{
+
+    products = [
+    new Product(
+        'A Pillow',
+        'https://th.bing.com/th/id/R.551b3f502f6c089c36c19e843c188dc9?rik=7%2brlsXe3dl0Bqw&riu=http%3a%2f%2f1.bp.blogspot.com%2f-QvL28cHZ-jw%2fU6xv6wD48KI%2fAAAAAAAAiy0%2fvTqlIJXTwp8%2fs1600%2fpillows%2b5.jpg&ehk=ZGjGWU1qByooSu%2fNM%2b0iWWs%2bub3FgJk0%2bb6BexNaSOk%3d&risl=&pid=ImgRaw&r=0',
+        19.99,
+        'a product description'
+
+    ),
+    new Product(
+        'A Pillow',
+        'https://th.bing.com/th/id/R.95dc5e66f6bed801dd0f00749244ae5b?rik=jPRBIdbTC1p0nQ&riu=http%3a%2f%2fwww.vineos.fr%2fvins%2fofficiel%2fvetement-sport-nike-femme-pas-cher-yxtonu.jpg&ehk=6aVUywyu%2fQ2o8ZaBCDg%2b%2fNmDJZHMbLVhpDaDnQm1Yvo%3d&risl=&pid=ImgRaw&r=0', 
+         19.99,
+        'a product description'
+
+    )    
+];
+constructor(){}
+
 render(){
     const renderHook = document.getElementById('app');
     const productList = document.createElement('ul');
@@ -37,7 +59,13 @@ render(){
          renderHook.append(productList);
     }
 }
-};
+
+
+}
+
+
+
+
 productList.render()
 
-
+//5. Working with Constructor Methods
